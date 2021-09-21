ee8a349af0ee3a985fe3c843d2d2c70e619bd9da
Stanley Pan <stanleypan.egre@gmail.com>
2021-09-20 22:08:12 -0400

    Initial Commit

---
+++ iTinder_Build/iTinder_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTinder_BuildApp.swift
+//  iTinder_Build
+//
+//  Created by Stanley Pan on 2021/09/20.
+//
+
+import SwiftUI
+
+@main
+struct iTinder_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
