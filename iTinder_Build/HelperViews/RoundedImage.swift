bf6bb30b56fc27b311ce84264247cd3328f01a7d
Stanley Pan <stanleypan.egre@gmail.com>
2021-09-21 00:26:15 -0400

    Import KingFisherUI, added RoundedImage, added and implemented on top half of ProfileView, added Color extensions

---
+++ iTinder_Build/HelperViews/RoundedImage.swift
@@ -0,0 +1,26 @@
+//
+//  RoundedImage.swift
+//  iTinder_Build
+//
+//  Created by Stanley Pan on 2021/09/20.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct RoundedImage: View {
+    var url: URL?
+    
+    var body: some View {
+        KFImage(url)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+    }
+}
+
+struct RoundedImage_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedImage(url: URL(string: "https://picsum.photos/400"))
+    }
+}
